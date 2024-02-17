db46504142b9b179be4d2bdd0033da13ab294071
freakster00 <rohan7poudel@gmail.com>
2024-02-17 09:07:48 +0545

    FlaskApplication

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/quote', methods=['GET'])
+def hello_world():
+    api_url = 'https://dog-api.kinduff.com/api/facts?number=1'
+
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            responseFacts={
+                "Fact":data['facts'][0]
+            }
+            return jsonify(responseFacts)
+        else:
+            errorMessage={
+            "Success":False,
+            "Message":f'Request failed with status code {response.status_code}'
+            }
+
+            return jsonify(errorMessage), response.status_code
+
+    except requests.exceptions.RequestException as e:
+        errorMessage={
+            "Success":False,
+            "Message":f'Request failed: {str(e)}'
+        }
+        return jsonify(errorMessage), 500
+    
+    
